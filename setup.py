4c36af8223f9830b873d9552bf7886acfd3b5207
Boris <boris2000n@gmail.com>
2021-03-11 11:07:34 +0300

    python lib setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bestconfig',
+    packages=find_packages(include=['bestconfig']),
+    version='0.1.0',
+    description="""Setup your project config easily""",
+    author='Boris Bondarenko',
+    license='MIT',
+)
