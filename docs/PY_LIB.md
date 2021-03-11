3dcbafd41737a2fbd33b0384c247d60c20485b17
Boris <boris2000n@gmail.com>
2021-03-11 13:42:56 +0300

    prepare package to publish

--- docs/PY_LIB.md
+++ docs/PY_LIB.md
@@ -1,3 +1,16 @@
 ## Тут описан проект в качестве python библиотеки
 
-[Статья](https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f)
+[Статья по созданию пакета](https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f)
+
+[Статья по публикации](https://realpython.com/pypi-publish-python-package/)
+
+Запустить тесты из папки `tests`
+```
+python setup.py pytest
+```
+Собрать библиотеку в папку `dist`
+```
+python setup.py bdist_wheel
+или 
+python setup.py sdist bdist_wheel
+```
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+import pathlib
+
 from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(
     name='bestconfig',
     packages=find_packages(include=['bestconfig']),
-    version='0.1.0',
+    version='1.0.0',
     description="""Setup your project config easily""",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fivol/bestconfig",
     author='Boris Bondarenko',
+    author_email="borisoffficial@gmail.com",
     license='MIT',
+    install_requires=[
+        'PyYAML==5.4.1',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==6.2.2'],
+    test_suite='tests',
 )
