6cb06ac7c27486e59e10212a497e3dd1de944c77
Boris <boris2000n@gmail.com>
2021-03-14 13:08:29 +0300

    fix example

--- examples/complex/config.py
+++ examples/complex/config.py
@@ -2,9 +2,10 @@
 Основной конфигурационный файл для проекта
 """
 
-from bestconfig import Config
+from bestconfig import Config, Source
 
-config = Config('dev.env')
+config = Config('dev.env', exclude=[Source.env])
 
 config.assert_contains('VALID')
 
+print(config)
