b7e3004d5d1d04383d5d4dff95b650e721a6a32e
Boris <boris2000n@gmail.com>
2021-03-27 10:10:52 +0300

    v 1.3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='bestconfig',
     packages=find_packages(include=['bestconfig']),
-    version='1.3.1',
+    version='1.3.2',
     description="""Setup your project config easily""",
     long_description=README,
     long_description_content_type="text/markdown",
